ca3d351eb8f014d5aa2fe74f61b20cbc299d1afe
sophia1215 <code.sophiawang@gmail.com>
2018-09-16 16:08:10 +0800

    Data Store

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,43 @@
 import React, { Component, Fragment } from "react";
 import { Header, Footer } from "./Layouts";
 import Exercises from "./Exercises"
+import { muscles, exercises } from "../store.js"
 
 class App extends Component {
+  state = {
+    exercises,
+  }
+
+  getExercisesByMuscles() {
+    // return this.state.exercises.reduce((exercises, exercise) => {
+    return Object.entries(
+      this.state.exercises.reduce((exercises, exercise) => {
+        const { muscles } = exercise
+
+        exercises[muscles] = exercises[muscles] 
+          ? [...exercises[muscles], exercise]
+          : [exercise]
+
+        return exercises
+      }, {})
+    )  
+  }
+
   render() {
+    // console.log(this.getExercisesByMuscles())
+    const exercises = this.getExercisesByMuscles()
+
     return (
       <Fragment>
         <Header />
-        <Exercises />
-        <Footer />
+
+        <Exercises 
+          exercises = { exercises }
+        />
+
+        <Footer 
+          muscles = { muscles }
+        />
       </Fragment>
     );
   }
